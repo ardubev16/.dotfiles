f0eaa8fae61c983ca9d637e58145ea4bc51ce979
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-02-03 16:03:19 +0100

    WIP: new install script

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo apt install zsh curl
+# install nix
+sh <(curl -L https://nixos.org/nix/install)
+
+# source nix
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+        nixpkgs.zsh \
+        nixpkgs.git \
+        nixpkgs.neovim \
+        nixpkgs.tmux \
+        nixpkgs.stow \
+        nixpkgs.fzf \
+        nixpkgs.bat \
+        nixpkgs.gcc \
+
+
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 mv ~/.zshrc ~/.zshrc.old
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-ln -s $SCRIPTPATH/.zshrc ~/.zshrc
