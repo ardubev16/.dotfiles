21b5681e1200acfb42f729e4820cece00d03b569
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-03-04 00:55:48 +0100

    python aliases, vim like exit

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,15 +2,19 @@ VIM="nvim"
 export GIT_EDITOR=$VIM
 
 
-# sourced from ThePrimogen/.config/personal/alias
+# edit .dotfiles
+alias ezsh="$VIM $DOTFILES/zsh/.config/zsh"
+alias evim="$VIM $DOTFILES/nvim/.config/nvim"
+alias etmx="$VIM $DOTFILES/tmux/.tmux.conf"
+
 # misc
 alias vim="$VIM"
 alias ivm="$VIM" # because I can't type
 alias xsc="xclip -selection clipboard"
 alias abup="antibody bundle < $ZDOTDIR/plugins.txt > $ZDOTDIR/.plugins"
 alias ez="exec zsh"
-# edit .dotfiles
-alias ezsh="$VIM $DOTFILES/zsh/.config/zsh"
-alias evim="$VIM $DOTFILES/nvim/.config/nvim/init.vim"
-alias etmx="$VIM $DOTFILES/tmux/.tmux.conf"
+alias :q="exit" # vim goodness
 
+# python alias
+alias py3="python3"
+alias py2="python2"
