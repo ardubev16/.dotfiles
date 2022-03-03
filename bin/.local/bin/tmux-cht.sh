2b52abf19da47ee75fcfb68400bb65ba7f30a6f7
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-03-04 00:52:01 +0100

    moved .tmux dir to .config/tmux

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux/tmux-cht-languages ~/.tmux/tmux-cht-commands | fzf`
+selected=`cat $HOME/.config/tmux/tmux-cht-languages $HOME/.config/tmux/tmux-cht-commands | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux/tmux-cht-languages; then
+if grep -qs "$selected" $HOME/.config/tmux/tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
