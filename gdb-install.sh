e4ad9e429cef9b4074953e7313a1122f52db2e19
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2022-06-27 15:25:58 +0200

    rudimentary gdb dependencies installation

---
+++ gdb-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -z "$DOTFILES" ]]; then
+    echo "DOTFILES not set!"
+    exit 1
+fi
+
+
+pushd $DOTFILES/gdb/.config/gdb
+
+git clone https://github.com/hugsy/gef REPO-gef
+git clone https://github.com/pwndbg/pwndbg REPO-pwndbg
+git clone https://github.com/jerdna-regeiz/splitmind REPO-splitmind
+
+./REPO-pwndbg/setup.sh
+
+pip3 install r2pipe
