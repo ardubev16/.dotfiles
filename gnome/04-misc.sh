2393129e27bbcf26ff58e9a9cd8b95f17ab92210
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-01-05 22:06:12 +0100

    feat(gnome): disable edge-tiling

--- gnome/04-misc.sh
+++ gnome/04-misc.sh
@@ -8,3 +8,4 @@ dconf write /org/gnome/desktop/peripherals/keyboard/repeat-interval "uint32 20"
 dconf write /org/gnome/desktop/sound/allow-volume-above-100-percent true
 dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
 dconf write /org/gnome/settings-daemon/plugins/color/night-light-temperature "uint32 3700"
+dconf write /org/gnome/mutter/edge-tiling false
