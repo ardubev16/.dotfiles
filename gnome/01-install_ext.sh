5c23207f8deecb94178f8b92d7078ce3f10a73ea
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-02 23:59:23 +0100

    feat: move around gnome extensions

--- gnome/01-install_ext.sh
+++ gnome/01-install_ext.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 extensions=(
-    7    # https://extensions.gnome.org/extension/7/removable-drive-menu/
-    16   # https://extensions.gnome.org/extension/16/auto-move-windows/
-    600  # https://extensions.gnome.org/extension/600/launch-new-instance/
     615  # https://extensions.gnome.org/extension/615/appindicator-support/
-    1319 # https://extensions.gnome.org/extension/1319/gsconnect/
-    1460 # https://extensions.gnome.org/extension/1460/vitals/
-    3193 # https://extensions.gnome.org/extension/3193/blur-my-shell/
-    3843 # https://extensions.gnome.org/extension/3843/just-perfection/
-    3956 # https://extensions.gnome.org/extension/3956/gnome-fuzzy-app-search/
-    4481 # https://extensions.gnome.org/extension/4481/forge/
-    5489 # https://extensions.gnome.org/extension/5489/search-light/
+    16   # https://extensions.gnome.org/extension/16/auto-move-windows/
     5724 # https://extensions.gnome.org/extension/5724/battery-health-charging/
+    4481 # https://extensions.gnome.org/extension/4481/forge/
+    3956 # https://extensions.gnome.org/extension/3956/gnome-fuzzy-app-search/
+    1319 # https://extensions.gnome.org/extension/1319/gsconnect/
+    3843 # https://extensions.gnome.org/extension/3843/just-perfection/
+    600  # https://extensions.gnome.org/extension/600/launch-new-instance/
+    7    # https://extensions.gnome.org/extension/7/removable-drive-menu/
+    5489 # https://extensions.gnome.org/extension/5489/search-light/
+    1460 # https://extensions.gnome.org/extension/1460/vitals/
     5967 # https://extensions.gnome.org/extension/5967/workspaces-indicator-by-open-apps/
 )
 
