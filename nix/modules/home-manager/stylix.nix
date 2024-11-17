4206533be5e51c7ff76ffce0fcfeefce0caa5c9f
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-18 00:45:30 +0100

    feat(nix): kitty with nix

---
+++ nix/modules/home-manager/stylix.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  stylix.enable = true;
+  stylix.polarity = "dark";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+
+  stylix.image = pkgs.fetchurl {
+    url = "https://i.pinimg.com/originals/ca/1b/30/ca1b30088eaa6560aa4e135eac294e73.jpg";
+    sha256 = "8d2e34bbfcb986d1a0cca3c1f34ca393dcfdb4b2470c2c97a48f1dcef03ca967";
+  };
+
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+      name = "JetBrains Mono Nerd Font";
+    };
+  };
+}
