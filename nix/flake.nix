b5dd0fa8df108c7a89ae40a4cb4a9723716671e0
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-18 00:45:30 +0100

    feat(nix): kitty with nix

---
+++ nix/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix.url = "github:danth/stylix";
+  };
+
+  outputs = { nixpkgs, home-manager, stylix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."ardubev_16" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./modules/home-manager/home.nix
+          ./modules/home-manager/stylix.nix
+          stylix.homeManagerModules.stylix
+        ];
+      };
+    };
+}
