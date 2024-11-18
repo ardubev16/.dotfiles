34e4ad905fb18e035bd8b531ebd0d4ae3b7351b5
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-18 22:40:37 +0100

    wip: not working stuff

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix.url = "github:danth/stylix";
