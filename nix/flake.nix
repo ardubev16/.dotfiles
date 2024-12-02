9b14cc887d3633531b4b6484f09a509eb6e3e423
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-02 03:03:52 +0100

    chore: update flake.lock

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,16 +2,16 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # HACK: 24.11 instead of unstable until `nerd-fonts.jetbrains-mono` is fixed in stylix
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixGL = {
-      url = "github:nix-community/nixGL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix.url = "github:danth/stylix";
+
+    nixGL.url = "github:nix-community/nixGL";
+
+    stylix.url = "github:danth/stylix/release-24.05";
   };
 
   outputs = { nixpkgs, home-manager, stylix, nixGL, ... }:
