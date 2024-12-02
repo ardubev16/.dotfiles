d1ce2708d596f04a8b1d8ddfbbbab9a44044a85d
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-03 00:55:53 +0100

    feat: add gnome extensions config

---
+++ nix/desktops/gnome/extensions.nix
@@ -0,0 +1,79 @@
+{ pkgs, lib, ... }:
+{
+  programs.gnome-shell = {
+    extensions = with pkgs.gnomeExtensions; [
+      { package = appindicator; }
+      { package = auto-move-windows; }
+      { package = forge; }
+      { package = fuzzy-app-search; }
+      { package = gsconnect; }
+      { package = just-perfection; }
+      { package = launch-new-instance; }
+      { package = removable-drive-menu; }
+      { package = search-light; }
+      { package = vitals; }
+      { package = workspaces-indicator-by-open-apps; }
+    ];
+  };
+
+  dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/shell/extensions/auto-move-windows" = {
+      application-list = [
+        "kitty.desktop:1"
+        "brave-browser.desktop:2" "io.github.zen_browser.zen.desktop:2"
+        "virtualbox.desktop:3"
+        "com.spotify.Client.desktop:7"
+        "org.mozilla.Thunderbird.desktop:8" "mozilla-thunderbird.desktop:8"
+        "discord.desktop:9" "com.discordapp.Discord.desktop:9"
+        "org.telegram.desktop.desktop:10"
+      ];
+    };
+
+    "org/gnome/shell/extensions/forge" = {
+      window-gap-hidden-on-single = true;
+      window-gap-size = mkUint32 6;
+    };
+    "org/gnome/shell/extensions/forge/keybindings" = {
+      prefs-tiling-toggle = [];
+      focus-border-toggle = [];
+      window-swap-last-active = [];
+      window-toggle-float = [ "<Super>f" ];
+      window-toggle-always-float = [ "<Shift><Super>f" ];
+    };
+
+    "org/gnome/shell/extensions/just-perfection" = {
+      animation = 4;
+      notification-banner-position = 2;
+      activities-button = false;
+    };
+
+    "org/gnome/shell/extensions/search-light" = {
+      shortcut-search = [ "<Super>space" ];
+      popup-at-cursor-monitor = true;
+      border-radius = 1.0;
+      scale-width = 0.2;
+      scale-height = 0.4;
+      font-size = 0;
+      entry-font-size = 1;
+      background-color = mkTuple [0.2 0.2 0.2 1.0];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-input-source = [];
+      switch-input-source-backward = [];
+    };
+
+    "org/gnome/shell/extensions/vitals" = {
+      hot-sensors = [
+        "_temperature_thinkpad_cpu_"
+        "_processor_usage_"
+        "_memory_allocated_"
+        "_storage_free_"
+      ];
+    };
+
+    "org/gnome/shell/extensions/workspaces-indicator-by-open-apps" = {
+      indicator-round-borders = false;
+      indicator-hide-empty = true;
+    };
+  };
+}
