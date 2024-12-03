533c35ed50036ef37ef80b3d0910c63943b9d2c4
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-03 19:01:30 +0100

    feat: cleanup zsh plugins

--- nix/applications/shell.nix
+++ nix/applications/shell.nix
@@ -55,15 +55,11 @@
     antidote = {
       enable = true;
       plugins = [
-        "lukechilds/zsh-nvm"
-
         "ohmyzsh/ohmyzsh"
         "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
         "ohmyzsh/ohmyzsh path:plugins/command-not-found"
         "ohmyzsh/ohmyzsh path:plugins/docker kind:fpath"
         "ohmyzsh/ohmyzsh path:plugins/git"
-        "ohmyzsh/ohmyzsh path:plugins/node"
-        "ohmyzsh/ohmyzsh path:plugins/npm"
         "ohmyzsh/ohmyzsh path:plugins/virtualenv"
         "ohmyzsh/ohmyzsh path:plugins/web-search"
         "ohmyzsh/ohmyzsh path:plugins/zsh-interactive-cd"
