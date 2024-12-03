c48c747f5122551c9ffeb359bbe750321f8d4b40
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-04 00:25:42 +0100

    refactor: move application config inside application folder

--- applications/shell/default.nix
+++ applications/shell/default.nix
@@ -20,7 +20,6 @@
     xsel
     xxd
     zip
-    (writeShellScriptBin "tmux-sessionizer" (builtins.readFile ../bins/tmux-sessionizer))
   ];
 
   home.shellAliases = {
@@ -80,49 +79,5 @@
     enable = true;
     enableZshIntegration = true;
   };
-  xdg.configFile."starship.toml".source = ../dotfiles/starship.toml;
-
-  programs.tmux = {
-    enable = true;
-    baseIndex = 1;
-    customPaneNavigationAndResize = true;
-    escapeTime = 0;
-    historyLimit = 50000;
-    keyMode = "vi";
-    mouse = true;
-    shortcut = "a";
-
-    plugins = with pkgs; [
-      {
-        plugin = tmuxPlugins.mode-indicator;
-        extraConfig = /*tmux*/ ''
-          # Theme
-          source-file ${../dotfiles/tmux-gruvbox-dark.conf}
-        '';
-      }
-    ];
-
-    extraConfig = /*tmux*/ ''
-      set -g renumber-windows on
-      set -g default-terminal "tmux-256color"
-      # undercurl support
-      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-      # support colors for undercurl
-      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
-
-      # Split panes using | and -
-      bind \\ split-window -h -c '#{pane_current_path}'
-      bind | split-window -v -c '#{pane_current_path}'
-      unbind '"'
-      unbind %
-
-      # Custom binds
-      bind i run-shell 'tmux neww tmux-cht.sh'
-      bind A run-shell "tmux neww tmux-sessionizer -a"
-      bind W run-shell "tmux neww tmux-sessionizer -w"
-      bind P run-shell "tmux neww tmux-sessionizer -p"
-      bind S run-shell "tmux neww tmux-sessionizer -u"
-      bind C run-shell "tmux neww tmux-sessionizer -c"
-    '';
-  };
+  xdg.configFile."starship.toml".source = ./starship.toml;
 }
