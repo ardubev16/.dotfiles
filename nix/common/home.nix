49dbc1d48fa4ded6faa0cebd7d1b7e4cf762c476
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-03 19:10:35 +0100

    feat: add packages

--- nix/common/home.nix
+++ nix/common/home.nix
@@ -17,27 +17,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # bat
-    # cmake
-    # curl
-    # fd
-    # fzf
-    # gcc
-    # gdb
-    # git
-    # gpp
-    # jq
-    # keyd
-    # lazygit
-    # nh
-    # nushell
-    # pipx
-    # ripgrep
-    # wget
-    # xclip
-    # xsel
-    # xxd
-    # zip
+    docker
+    keyd
+    kubectl
+    nushell
   ];
 
   programs.home-manager.enable = true;
