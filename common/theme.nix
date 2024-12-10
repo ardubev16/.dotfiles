07d5b62d1343d4d87c4dc9ce9de9578d6b5a1b53
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-11 00:23:28 +0100

    feat: add icon theme to stylix

--- common/theme.nix
+++ common/theme.nix
@@ -3,6 +3,7 @@
 {
   stylix.enable = true;
   stylix.autoEnable = false;
+
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
@@ -17,4 +18,22 @@
       name = "JetBrains Mono Nerd Font";
     };
   };
+
+  # gtk = {
+  #   iconTheme = {
+  #     name = "Papirus-Dark";
+  #     package = pkgs.papirus-icon-theme;
+  #   };
+  #   gtk3.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  #   gtk4.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  # };
+  # qt = {
+  #   enable = true;
+  #   style.name = "adwaita-dark";
+  #   platformTheme.name = "gtk3";
+  # };
 }
