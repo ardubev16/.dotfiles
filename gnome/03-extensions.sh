54af30e0fd2b4a43506f6528ca8246786edeb107
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-01-07 19:38:32 +0100

    feat: kanata config + pop-shell extension

--- gnome/03-extensions.sh
+++ gnome/03-extensions.sh
@@ -2,9 +2,10 @@
 
 # auto-move-windows
 dconf write /org/gnome/shell/extensions/auto-move-windows/application-list "[\
-    'Alacritty.desktop:1', \
-    'google-chrome.desktop:2', \
+    'kitty.desktop:1', \
+    'brave-browser.desktop:2', 'io.github.zen_browser.zen.desktop:2' \
     'virtualbox.desktop:3', \
+    'com.spotify.Client.desktop:7', \
     'org.mozilla.Thunderbird.desktop:8', 'mozilla-thunderbird.desktop:8', \
     'discord.desktop:9', 'com.discordapp.Discord.desktop:9', \
     'org.telegram.desktop.desktop:10', 'whatsdesk.desktop:10'\
@@ -31,14 +32,28 @@ dconf write /org/gnome/shell/extensions/search-light/font-size 0
 dconf write /org/gnome/shell/extensions/search-light/entry-font-size 1
 dconf write /org/gnome/shell/extensions/search-light/background-color "(0.2, 0.2, 0.2, 1.0)"
 
+# pop-shell
+dconf write /org/gnome/shell/extensions/pop-shell/fullscreen-launcher true
+dconf write /org/gnome/shell/extensions/pop-shell/pop-monitor-left "['']"
+dconf write /org/gnome/shell/extensions/pop-shell/pop-monitor-right "['']"
+dconf write /org/gnome/shell/extensions/pop-shell/pop-workspace-down "['']"
+dconf write /org/gnome/shell/extensions/pop-shell/pop-workspace-up "['']"
+dconf write /org/gnome/shell/extensions/pop-shell/smart-gaps true
+dconf write /org/gnome/shell/extensions/pop-shell/tile-by-default true
+dconf write /org/gnome/shell/extensions/pop-shell/tile-enter "['']"
+dconf write /org/gnome/shell/extensions/pop-shell/tile-move-down-global "['<Shift><Super>j']"
+dconf write /org/gnome/shell/extensions/pop-shell/tile-move-left-global "['<Shift><Super>h']"
+dconf write /org/gnome/shell/extensions/pop-shell/tile-move-right-global "['<Shift><Super>l']"
+dconf write /org/gnome/shell/extensions/pop-shell/tile-move-up-global "['<Shift><Super>k']"
+
 # forge
-dconf write /org/gnome/shell/extensions/forge/window-gap-hidden-on-single true
-dconf write /org/gnome/shell/extensions/forge/window-gap-size "uint32 6"
-dconf write /org/gnome/shell/extensions/forge/keybindings/prefs-tiling-toggle "['']"
-dconf write /org/gnome/shell/extensions/forge/keybindings/focus-border-toggle "['']"
-dconf write /org/gnome/shell/extensions/forge/keybindings/window-swap-last-active "['']"
-dconf write /org/gnome/shell/extensions/forge/keybindings/window-toggle-float "['<Super>f']"
-dconf write /org/gnome/shell/extensions/forge/keybindings/window-toggle-always-float "['<Shift><Super>f']"
+# dconf write /org/gnome/shell/extensions/forge/window-gap-hidden-on-single true
+# dconf write /org/gnome/shell/extensions/forge/window-gap-size "uint32 6"
+# dconf write /org/gnome/shell/extensions/forge/keybindings/prefs-tiling-toggle "['']"
+# dconf write /org/gnome/shell/extensions/forge/keybindings/focus-border-toggle "['']"
+# dconf write /org/gnome/shell/extensions/forge/keybindings/window-swap-last-active "['']"
+# dconf write /org/gnome/shell/extensions/forge/keybindings/window-toggle-float "['<Super>f']"
+# dconf write /org/gnome/shell/extensions/forge/keybindings/window-toggle-always-float "['<Shift><Super>f']"
 
 # vitals
 dconf write /org/gnome/shell/extensions/vitals/hot-sensors "['_temperature_thinkpad_cpu_', '_processor_usage_', '_memory_allocated_', '_storage_free_']"
--- installation/config.lua
+++ installation/config.lua
@@ -6,6 +6,7 @@ local M = {
         'nvim',
         'tmux',
         'zsh',
+        'kanata',
     },
 }
 
