6716df6fd7bf6b12e207795bc7cb5461cc9fd144
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-07-01 19:26:57 +0200

    feat: add setup script for kmonad

---
+++ kmonad/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+main() {
+    if ! command -v kmonad &>/dev/null; then
+        echo "kmonad is not installed"
+        exit 1
+    fi
+
+    local -r kmonad_path=$(dirname "$(realpath "$0")")
+    local -r service_file_path="$kmonad_path/hosts/$(hostname).kmonad.service"
+
+    if [[ ! -f "$service_file_path" ]]; then
+        echo "Service file not found for current host: $service_file_path"
+        exit 1
+    fi
+
+    sudo cp "$service_file_path" /etc/systemd/system/kmonad.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable kmonad.service
+    sudo systemctl start kmonad.service
+}
+
+main
