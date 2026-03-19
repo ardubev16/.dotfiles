7fdc3adeb08abeb4451c474258074807e815d8cb
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2026-03-19 21:41:22 +0000

    chore: remove no-longer needed packages from work profile

--- hosts/work/users/lbevilac.nix
+++ hosts/work/users/lbevilac.nix
@@ -1,53 +1,31 @@
 {
   inputs,
-  perSystem,
   pkgs,
   ...
 }:
-let
-  unstable-pkgs = with perSystem.nixpkgs-unstable; [
-    awscli2 # >= 2.32.15
-  ];
-in
 {
   imports = [ inputs.self.homeModules.baseUser ];
 
   global.notNixOS = true;
 
   global.toolsets.kubernetes = true;
-  global.toolsets.security = true;
   global.toolsets.ai = true;
 
   home.packages =
     with pkgs;
     [
-      azure-cli
       git-filter-repo
       git-lfs
       gnumake
-      hclfmt
       htop
-      kmonad
       nushell
       powershell
       prek
-      spotify
-      teams-for-linux
-      telegram-desktop
-      terraform
-      terragrunt
-      tflint
       trivy
       uv
       yarn
       yazi
-    ]
-    ++ [
-      crc
-      openshift
-      tektoncd-cli
-    ]
-    ++ unstable-pkgs;
+    ];
 
   home.shellAliases = {
     # Open Ansible logs in a temporary file from the clipboard, also add newlines
