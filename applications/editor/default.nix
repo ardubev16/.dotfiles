e71447f655f8cab771434b5f3cb34535c37ebe51
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2026-06-08 21:50:23 +0100

    fix: neovim config workaround

--- applications/editor/default.nix
+++ applications/editor/default.nix
@@ -29,12 +29,8 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+    sideloadInitLua = true;
   };
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/applications/editor/nvim";
-
-  # FIXME: Prevent home-manager's neovim module from generating nvim/init.lua,
-  # which conflicts with our custom config directory symlink above.
-  # src: https://github.com/nix-community/home-manager/issues/5982#issuecomment-4615838595
-  xdg.configFile."nvim/init.lua".enable = pkgs.lib.mkForce false;
 }
